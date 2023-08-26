19b962a0c0750d1555d02b4b10464a8d74d2aa13
lbarrowlb <lbarrowlb@gmail.com>
2023-08-26 12:47:52 -0500

    prepare to submit

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -1,108 +1,81 @@
-// Store our API endpoint as url
-let url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson'
-
-
+//  Read in the eartquake data
+let scale = 10000
+let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
 d3.json(url).then(function (data) {
-    popFeatures(data.features);
-});
 
-function markerSize(magnitude) {
-    return magnitude * 5;
-}
+  let myMap = L.map("map", {
+    center: [15.5994, -28.6731],
+    zoom: 3
 
+  });
 
-function markerColor(magnitude) {
-    if (magnitude <= -1) {
-      return "#79ff4d";
-    } else if (magnitude <= 2) {
-      return "#d2ff4d";
-    } else if (magnitude <= 3) {
-      return "#ffff4d";
-    } else if (magnitude <= 4) {
-      return "#ffd24d";
-    } else if (magnitude <= 5) {
-      return "#ffa64d";
-    } else if (magnitude <= 6) {
-      return "#ff794d";
-    } else {
-      return "#ff4d4d";
+  // Add a tile layer.
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(myMap);
+
+  data.features.map((value, index) => {
+
+    let geometry = value.geometry;
+    // console.log("coordinates =" + JSON.stringify(geometry.coordinates));
+    let coordinates = geometry.coordinates;
+    let longitude = coordinates[0];
+    let latitude = coordinates[1];
+    let altitude = coordinates[2];
+    let coord = {lat: latitude, lng: longitude};
+    let magnitude = value.properties.mag;
+    let location = value.properties.place;
+    
+
+    // console.log("longitude = " + longitude);
+   
+//  Get the depth color
+    function calculateColor(altitude) {
+      if (altitude > 90)
+        return "#f25c64"
+      else if (altitude > 70)
+        return "#f9a25d"
+      else if (altitude > 50)
+        return "#fdb733"
+      else if (altitude > 30)
+        return "#f6db38"
+      else if (altitude > 10)
+        return "#daed39"
+      else if (altitude >= -10)
+        return "#9eea33"
     }
+
+
+    // Add circles to the map.
+    L.circle(coord, {
+      opacity: 1,
+      fillOpacity: 1,
+      color: "black",
+      fillColor: calculateColor(altitude),
+      // Adjust the radius.
+      radius: scale * magnitude,
+      stroke: true,
+      weight:0.5
+    }).bindPopup("<b>Location: </b>" + location + "<br/><b>Magnitude:</b> " + magnitude).addTo(myMap);
+   console.log(location, coord, magnitude) 
+
+ });
+
+ //   Create Legend for the map
+ let legend = L.control({position: "bottomright"});
+ legend.onAdd = function () {
+   let div = L.DomUtil.create("div", "info legend");
+   
+   let grades =[-10, 10, 30, 50, 70, 90];
+   let colors = ["#9eea33","#daed39", "#f6db38", "#fdb733","#f9a25d","#f25c64"];
+  
+   for (let i = 0; i < grades.length; i++) {
+    div.innerHTML += "<i style='background: " + colors[i] + "'></i> " + grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
+  }
+  return div;
 };
+//  Add legend to map
+   legend.addTo(myMap);
 
-function popFeatures(earthquake) {
 
-    
-    function onEachFeature(feature, layer) {
-      layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>Magnitude - ${feature.properties.mag} </p>`);
-    }
-  
-    let earthquakes = L.geoJSON(earthquake, {
-        pointToLayer: function(feature, latlng) {
-            return L.circleMarker(latlng, {
-                radius: markerSize(feature.properties.mag),
-                fillColor: markerColor(feature.properties.mag),
-                color: "#000",
-                weight: 0.3,
-                opacity: 1,
-                fillOpacity: 1
-            });
-        },
-        onEachFeature: onEachFeature
-    });
-  
-    
-    popMap(earthquakes);
-  }
-  
-  function popMap(earthquakes) {
-  
-    let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    })
-  
-    let baseMaps = {
-      "Street Map": street
-    };
-    
-    let overlayMaps = {
-      Earthquakes: earthquakes
-    };
-  
-    let myMap = L.map("map", {
-      center: [
-        37.09, -95.71
-      ],
-      zoom: 5,
-      layers: [street, earthquakes]
-    });
-
-  
-    var legend = L.control({ position: "bottomright" });
-    legend.onAdd = function(map) {
-      var div = L.DomUtil.create("div", "info legend");
-      magnitudes = [1 , 2, 3, 4, 5, 6];
-      labels = [];
-      legendInfo = "<strong>Magnitude</strong>";
-      div.innerHTML = legendInfo;
-      
-      
-      for (var i = 0; i < magnitudes.length; i++) {
-        var magnitudeRange = magnitudes[i];
-        var nextMagnitude = magnitudes[i + 1];
-        var color = markerColor((magnitudeRange + nextMagnitude) / 2);
-        var label = '<li style="background-color:' + color + '"> <span>' + magnitudeRange + (nextMagnitude ? '&ndash;' + nextMagnitude : '+') + '</span></li>';
-        
-        labels.push(label);
-      }
-      
-      div.innerHTML += "<ul>" + labels.join("") + "</ul>";
-      return div;
-    };
-    
-    legend.addTo(myMap);
-
-    L.control.layers(baseMaps, overlayMaps, {
-      collapsed: false
-    }).addTo(myMap);
-  
-  }
+  });
